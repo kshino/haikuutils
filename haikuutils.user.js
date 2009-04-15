1adb18041c2bee0f9d463ac3aa3030a8a481fc08
id:khashi <ksk.hashi+github@gmail.com>
2009-04-15 18:02:42 +0900

    * add: loginLocation * add: addLogout

--- haikuutils.user.js
+++ haikuutils.user.js
@@ -3,13 +3,22 @@
 // @namespace      http://www.scrapcode.net/
 // @include        http://h.hatena.ne.jp/*
 // @include        http://h.hatena.com/*
-// @version        1.0.0
+// @version        1.2.0
 // ==/UserScript==
 (function() {
     // Select utility
     var runUtils = [
+        // 広告をわかりやすいように背景色をつける
         { name: 'adColoring', args: {} },
+
+        // 小さな画像が引き延ばされて大きくなる問題を解消する
         { name: 'imageNoResize', args: {} },
+
+        // ログイン後に、「ログイン」リンクをクリックしたページに戻る
+        { name: 'loginLocation', args: {} },
+
+        // ログアウトリンクの付加
+        { name: 'addLogout', args: {} },
     ];
     
     function xpath(context, query) {
@@ -46,7 +55,49 @@
                 img.removeAttribute( 'height' );
             }
         },
-    }
+    };
+
+    utils.loginLocation = {
+        initOnly: true,
+        func: function ( args ) {
+            var anchors = xpath( document.body, '//ul[@id="global-menu"]/li/a' );
+            var login   = 'https://www.hatena.ne.jp/login';
+            for( var i = 0; i < anchors.snapshotLength; ++i ) {
+                var anchor = anchors.snapshotItem(i);
+                if( anchor.href.indexOf( login ) == -1 ) continue;
+                anchor.href = login + '?location=' + document.location.href;
+                break;
+            }
+        },
+    };
+
+    utils.addLogout = {
+        initOnly: true,
+        func: function ( args ) {
+            var username = xpath( document.body, '//div[@id="header"]/p[@class="username"]' );
+            if( username.snapshotLength == 0 ) return;
+
+            var list = xpath( document.body, '//ul[@id="global-menu"]' );
+            var logout  = 'https://www.hatena.ne.jp/logout';
+
+            if( list.snapshotLength != 1 ) return;
+
+            var link = document.createElement( 'a' );
+            link.href = logout + '?location=' + document.location.href;
+            link.innerHTML = 'ログアウト';
+
+            var li = document.createElement( 'li' );
+            li.appendChild( link );
+
+            list.snapshotItem(0).appendChild( li );
+
+            username.snapshotItem(0).style.top = '75px';
+            var lists = xpath( document.body, '//ul[@id="global-menu"]/li' );
+            for( var i = 0; i < lists.snapshotLength; ++i ) {
+                lists.snapshotItem(i).style.lineHeight = '1.1em';
+            }
+        },
+    };
     
     for( var i = 0; i < runUtils.length; ++i ) {
         var target = runUtils[i];
