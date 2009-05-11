750cc711ea143f4fbac9cd0b318b4899749b0935
id:khashi <ksk.hashi+github@gmail.com>
2009-05-12 00:28:45 +0900

    * add お気に入りに追加時に確認するconfirmFollowAddを追加 * add docomoマップのURLをGoogleマップのURLに置換する docomoMapToGoogleMapを追加 * change replyToBlankをデフォルト無効にした * fix 英語版のログアウトリンクを「Sign Out」に表記変更 * リファクタリング

--- haikuutils.user.js
+++ haikuutils.user.js
@@ -3,7 +3,7 @@
 // @namespace      http://www.scrapcode.net/
 // @include        http://h.hatena.ne.jp/*
 // @include        http://h.hatena.com/*
-// @version        1.4.1
+// @version        1.5.0
 // ==/UserScript==
 (function() {
     // Select utility
@@ -21,19 +21,72 @@
         { name: 'addLogout', args: {} },
 
         // Reply投稿時、別ウィンドウに遷移
-        { name: 'replyToBlank', args: {} },
+//        { name: 'replyToBlank', args: {} },
 
         // お気に入りを解除する時に確認する
         { name: 'confirmFollowRemove', args: {} },
+
+        // お気に入りに追加する時に確認する
+        { name: 'confirmFollowAdd', args: {} },
+
+        // docomoマップのURLをGoogleマップのURLに置換する
+        { name: 'docomoMapToGoogleMap', args: {} },
     ];
 
     location.host.match( /\.hatena\.(.+)/ );
-    var DOMAIN = RegExp.$1;
+    const DOMAIN = RegExp.$1;
+    const GMAP   = 'http://maps.google.com/maps?q=';
 
-    function xpath(context, query) {
-        return document.evaluate(
+    function xpath( context, query ) {
+        var items = document.evaluate(
             query, context, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null
-        )
+        );
+
+        var elements = [];
+        for( var i = 0; i < items.snapshotLength; ++i ) {
+            elements.push( items.snapshotItem( i ) );
+        }
+
+        return elements;
+    }
+
+    function parseQueryParam( url, forceArrayKeys ) {
+        if( ! url.match( /\?(.*)/ ) ) return null;
+
+        var params = RegExp.$1.split( '&' );
+        var hash   = {};
+        var i;
+        if( typeof( forceArrayKeys ) == 'Array' ) {
+            for( i = 0; i < forceArrayKeys.length; ++i ) {
+                hash[ forceArrayKeys[i] ] = [];
+            }
+        }
+
+        for( i = 0; i < params.length; ++i ) {
+            var p   = params[i].split( '=' );
+            var key = decodeURIComponent( p[0] );
+            var val = decodeURIComponent( p[1] );
+            if( hash[key] ) {
+                if( typeof( hash[key] ) != 'Array' ) {
+                    hash[key] = [ hash[key] ];
+                }
+                hash[key].push( val );
+            }else {
+                hash[key] = val;
+            }
+        }
+
+        return hash;
+    }
+
+    function dmsToDeg( dms ) {
+        if( ! dms.match( /^([-+]?)(\d+)\.(\d+)\.(\d+\.\d+)$/ ) ) return null;
+
+        var pm  = RegExp.$1;
+        var deg = 1 * RegExp.$2 + RegExp.$3 / 60.0 + RegExp.$4 / 3600.0;
+        if( pm == '-' ) deg *= -1;
+
+        return Math.floor( deg * 1000000 ) / 1000000;
     }
 
     var utils = {};
@@ -42,8 +95,8 @@
         initOnly: false,
         func: function ( args ) {
             var ads = xpath( document.body, '//div[@class="entry ad"]' );
-            for( var i = 0; i < ads.snapshotLength; ++i ) {
-                var ad = ads.snapshotItem(i);
+            for( var i = 0; i < ads.length; ++i ) {
+                var ad = ads[i];
                 ad.style.background = args.color;
             }
         },
@@ -58,10 +111,10 @@
             );
 
             var imgs = xpath( document.body, '//div[@class="body"]//img' );
-            for( var i = 0; i < imgs.snapshotLength; ++i ) {
-                var img = imgs.snapshotItem(i);
-                img.removeAttribute( 'width' );
-                img.removeAttribute( 'height' );
+            for( var i = 0; i < imgs.length; ++i ) {
+                var img = imgs[i];
+                if( typeof( img['width']  ) != 'undefined' ) img.removeAttribute( 'width' );
+                if( typeof( img['height'] ) != 'undefined' ) img.removeAttribute( 'height' );
             }
         },
     };
@@ -71,8 +124,8 @@
         func: function ( args ) {
             var anchors = xpath( document.body, '//ul[@id="global-menu"]/li/a' );
             var login   = 'https://www.hatena.' + DOMAIN + '/login';
-            for( var i = 0; i < anchors.snapshotLength; ++i ) {
-                var anchor = anchors.snapshotItem(i);
+            for( var i = 0; i < anchors.length; ++i ) {
+                var anchor = anchors[i];
                 if( anchor.href.indexOf( login ) == -1 ) continue;
                 anchor.href = login + '?location=' + document.location.href;
                 break;
@@ -84,26 +137,26 @@
         initOnly: true,
         func: function ( args ) {
             var username = xpath( document.body, '//div[@id="header"]/p[@class="username"]' );
-            if( username.snapshotLength == 0 ) return;
+            if( username.length == 0 ) return;
 
             var list = xpath( document.body, '//ul[@id="global-menu"]' );
             var logout  = 'https://www.hatena.' + DOMAIN + '/logout';
 
-            if( list.snapshotLength != 1 ) return;
+            if( list.length != 1 ) return;
 
             var link = document.createElement( 'a' );
             link.href = logout + '?location=' + document.location.href;
-            link.innerHTML = 'ログアウト';
+            link.innerHTML = DOMAIN == 'ne.jp' ? 'ログアウト': 'Sign Out';
 
             var li = document.createElement( 'li' );
             li.appendChild( link );
 
-            list.snapshotItem(0).appendChild( li );
+            list[0].appendChild( li );
 
-            username.snapshotItem(0).style.top = '75px';
+            username[0].style.top = '75px';
             var lists = xpath( document.body, '//ul[@id="global-menu"]/li' );
-            for( var i = 0; i < lists.snapshotLength; ++i ) {
-                lists.snapshotItem(i).style.lineHeight = '1.1em';
+            for( var i = 0; i < lists.length; ++i ) {
+                lists[i].style.lineHeight = '1.1em';
             }
         },
     };
@@ -124,16 +177,45 @@
         initOnly: true,
         func: function ( args ) {
             var forms = xpath( document.body, '//form[@action="/follow.remove"]' );
-            if( forms.snapshotLength != 1 ) return;
+            if( forms.length != 1 ) return;
 
             var onsubmit = function ( e ) {
-                if( ! confirm( 'Stop following?' ) ) {
-                    e.preventDefault();
-                }
+                if( ! confirm( 'Stop following?' ) ) e.preventDefault();
             };
 
-            var form = forms.snapshotItem( 0 );
-            form.addEventListener( 'submit', onsubmit, true );
+            forms[0].addEventListener( 'submit', onsubmit, true );
+        },
+    };
+
+    utils.confirmFollowAdd = {
+        initOnly: true,
+        func: function ( args ) {
+            var forms = xpath( document.body, '//form[@action="/follow"]' );
+            if( forms.length != 1 ) return;
+
+            var onsubmit = function ( e ) {
+                if( ! confirm( 'Add favorites?' ) ) e.preventDefault();
+            };
+
+            forms[0].addEventListener( 'submit', onsubmit, true );
+        },
+    };
+
+    utils.docomoMapToGoogleMap = {
+        initOnly: false,
+        func: function ( args ) {
+            var links = xpath( document.body, '//div[@class="entry"]//div[@class="body"]//a' );
+            var reg_map = /^http:\/\/docomo\.ne\.jp\/cp\/map\.cgi\?/;
+            for( var i = 0; i < links.length; ++i ) {
+                var link = links[i];
+                if( ! link.href.match( reg_map ) ) continue;
+
+                var param = parseQueryParam( link.href );
+                var url   = GMAP + dmsToDeg( param.lat ) + ',' + dmsToDeg( param.lon );
+
+                link.href = url;
+                link.textContent = url;
+            }
         },
     };
 
@@ -144,12 +226,16 @@
             util.func( target.args );
             if( util.initOnly ) util.func = null;
         }
-        if( util.func && window.AutoPagerize ) {
-            window.AutoPagerize.addFilter(
-                function(){
-                    if( util.func ) util.func( target.args );
-                }
-            );
+
+        if( ! util.func ) continue;
+
+        var func = function() {
+            if( util.func ) util.func( target.args );
+        };
+        if( window.AutoPagerize ) {
+            window.AutoPagerize.addFilter( func );
+        }else {
+            unsafeWindow.Hatena.Haiku.Pager.addEventListener( 'loadedEntries', func );
         }
     }
 })();
